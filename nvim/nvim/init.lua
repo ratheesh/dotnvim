c41e01932263ee31af356e67944ddfe79de309e4
Ratheesh S <ratheeshreddy@gmail.com>
2023-01-01 23:50:45 +0530

    startup: move init code to lazy_bootstrap module

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,65 +1,18 @@
+-- Copyright (c) 2023 Ratheesh <ratheeshreddy@gmail.com>
+-- License: MIT
+-- Neovim bootstraping
+
 vim.opt.rtp:append(vim.fn.stdpath "config" .. "/config")
 
 require("config.settings")
 require("config/lazy_bootstrap")
 
-require("lazy").setup("plugins", {
- defaults = { lazy = true },
- -- install = { colorscheme = { "tokyonight" } },
- ui = {
-	 border = 'rounded',
- },
- checker = { enabled = true },
- performance = {
-  rtp = {
-   disabled_plugins = {
-    "gzip",
-    "matchit",
-    "matchparen",
-    "netrwPlugin",
-    "tarPlugin",
-    "tohtml",
-    "tutor",
-    "zipPlugin",
-   },
-  },
- },
- -- debug = true,
-})
-
 require("config.colors")
 vim.api.nvim_create_autocmd("User", {
- pattern = "VeryLazy",
- callback = function()
-  require("config.keymaps")
-  require("config.commands")
- end,
+	pattern = "VeryLazy",
+	callback = function()
+		require("config.keymaps")
+		require("config.commands")
+	end,
 })
-
--- don't load the plugins below
-local builtins = {
- "gzip",
- "zip",
- "zipPlugin",
- "fzf",
- "tar",
- "tarPlugin",
- "getscript",
- "getscriptPlugin",
- "vimball",
- "vimballPlugin",
- "2html_plugin",
- "matchit",
- "matchparen",
- "logiPat",
- "rrhelper",
- "netrw",
- "netrwPlugin",
- "netrwSettings",
- "netrwFileHandlers",
-}
-
-for _, plugin in ipairs(builtins) do
- vim.g["loaded_" .. plugin] = 1
-end
 -- End of File
--- nvim/nvim/lua/config/lazy_bootstrap.lua
+++ nvim/nvim/lua/config/lazy_bootstrap.lua
@@ -10,3 +10,50 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+	defaults = { lazy = true },
+	-- install = { colorscheme = { "tokyonight" } },
+	ui = {
+		border = 'rounded',
+	},
+	checker = { enabled = true, notify = false },
+	change_detection = { enabled = true, notify = false, },
+	performance = {
+		cache = {
+			enabled = true,
+			path = vim.fn.stdpath("cache") .. "/lazy/cache",
+			--   VimEnter: not useful to cache anything else beyond startup
+			--   BufReadPre: this will be triggered early when opening a file from the command line directly
+			disable_events = { "VimEnter", "BufReadPre" },
+			-- ttl = 3600  24  5, -- keep unused modules for up to 5 days
+		},
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"zip",
+				"zipPlugin",
+				"fzf",
+				"tar",
+				"tarPlugin",
+				"getscript",
+				"getscriptPlugin",
+				"vimball",
+				"vimballPlugin",
+				"2html_plugin",
+				"matchit",
+				"matchparen",
+				"logiPat",
+				"rrhelper",
+				"netrw",
+				"netrwPlugin",
+				"netrwSettings",
+				"netrwFileHandlers",
+				"tohtml",
+				"tutor",
+			},
+		},
+	},
+	-- debug = true,
+})
+
