6f183a8bf71dd0fa79ffbb48c94aff3600c6ef3d
Ratheesh S <ratheeshreddy@gmail.com>
2023-01-04 22:12:10 +0530

    port few autocmds from folke's dot

--- nvim/nvim/lua/config/autocmds.lua
+++ nvim/nvim/lua/config/autocmds.lua
@@ -43,6 +43,39 @@ callback = function(_)
 end
 })
 
+-- jump to last point of current buffer when opened
+--[[ vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+}) ]]
+
+-- create directories when needed, when saving a file
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    local backup = vim.fn.fnamemodify(file, ":p:~:h")
+    backup = backup:gsub("[/\\]", "%%")
+    vim.go.backupext = backup
+  end,
+})
+
+-- Fix conceallevel for json & help files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
+
 -- sqls
 vim.api.nvim_create_autocmd('User', {
 	pattern = 'SqlsConnectionChoice',
