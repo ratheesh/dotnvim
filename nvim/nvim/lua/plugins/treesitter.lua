c7dfdcf2dcca26c36310a62d0750de6ac03553eb
Ratheesh S <ratheeshreddy@gmail.com>
2023-01-09 19:05:58 +0530

    treesitter: disable query_linter

--- nvim/nvim/lua/plugins/treesitter.lua
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -47,7 +47,7 @@ function M.config()
 			},
 		},
     query_linter = {
-      enable = true,
+      enable = false,
       use_virtual_text = true,
       lint_events = { "BufWrite", "CursorHold" },
     },
