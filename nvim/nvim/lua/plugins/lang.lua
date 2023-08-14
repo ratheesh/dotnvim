f3994bd93c9cb5c90a52b8531c9ee983d56fd8c5
Ratheesh S <ratheeshreddy@gmail.com>
2023-08-14 22:50:12 +0530

    Add lang module

---
+++ nvim/nvim/lua/plugins/lang.lua
@@ -0,0 +1,54 @@
+-- Copyright (c) 2023 Ratheesh <ratheeshreddy@gmail.com>
+-- Author: Ratheesh S
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of
+-- this software and associated documentation files (the "Software"), to deal in
+-- the Software without restriction, including without limitation the rights to
+-- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+-- the Software, and to permit persons to whom the Software is furnished to do so,
+-- subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+-- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+-- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+-- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+-- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+return {
+	{
+		"p00f/clangd_extensions.nvim",
+		lazy = true,
+		config = function() end,
+		opts = {
+			extensions = {
+				inlay_hints = {
+					inline = false,
+				},
+				ast = {
+					--These require codicons (https://github.com/microsoft/vscode-codicons)
+					role_icons = {
+						type = "",
+						declaration = " ",
+						expression  = " ",
+						specifier   = " ",
+						statement   = " ",
+						["template argument"] = " ",
+					},
+					kind_icons = {
+						Compound = " ",
+						Recovery = " ",
+						TranslationUnit      = " ",
+						PackExpansion        = " ",
+						TemplateTypeParm     = " ",
+						TemplateTemplateParm = " ",
+						TemplateParamObject  = " ",
+					},
+				},
+			},
+		},
+	}
+}
