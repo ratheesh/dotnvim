f6ca9d8cc53ede37dbd155d2d01fe86327ce435d
Ratheesh S <ratheeshreddy@gmail.com>
2023-08-28 00:19:30 +0530

    feat(lsp): actions-preview plugin

--- nvim/nvim/lua/plugins/lsp/init.lua
+++ nvim/nvim/lua/plugins/lsp/init.lua
@@ -25,6 +25,28 @@ return {
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
             "williamboman/mason-lspconfig.nvim",
+			{
+				'aznhe21/actions-preview.nvim',
+				event = "LspAttach",
+				config = function()
+					require("actions-preview").setup {
+						telescope = {
+							sorting_strategy = "ascending",
+							layout_strategy  = "vertical",
+							layout_config = {
+								width  = 0.8,
+								height = 0.9,
+								prompt_position = "top",
+								preview_cutoff  = 20,
+								preview_height  = function(_, _, max_lines)
+									return max_lines - 15
+								end,
+							},
+						},
+					}
+					vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+				end
+			},
 			{ 'ray-x/lsp_signature.nvim', event = 'InsertEnter' },
 			{
 				"roobert/action-hints.nvim",
