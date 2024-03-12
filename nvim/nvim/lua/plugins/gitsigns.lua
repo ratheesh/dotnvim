aaadd5ca0c9e7436c20626bba8d0113e11ce3694
Ratheesh S <ratheeshreddy@gmail.com>
2024-03-12 10:15:01 +0530

    gitsigns: Add user commands for range staging and resetting
    New commands: Stage and Reset

--- nvim/nvim/lua/plugins/gitsigns.lua
+++ nvim/nvim/lua/plugins/gitsigns.lua
@@ -10,6 +10,7 @@ local M = {
 function M.config()
   require('gitsigns').setup({
     on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
       -- local gs = package.loaded.gitsigns
       local function map(mode, l, r, opts)
         opts = opts or {}
@@ -17,6 +18,8 @@ function M.config()
         vim.keymap.set(mode, l, r, opts)
       end
       map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      vim.api.nvim_create_user_command("Stage", function(t) gitsigns.stage_hunk({ t.line1, t.line2 }) end, { range = true })
+      vim.api.nvim_create_user_command("Reset", function(t) gitsigns.reset_hunk({ t.line1, t.line2 }) end, { range = true })
     end,
     signs = {
       add          = { text = "│" },
