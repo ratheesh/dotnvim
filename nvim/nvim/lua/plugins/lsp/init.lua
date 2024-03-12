977077b3b1eb666ee03dc2386e56854e16b2f4b3
Ratheesh S <ratheeshreddy@gmail.com>
2024-03-12 10:15:01 +0530

    lsp: configure mason

--- nvim/nvim/lua/plugins/lsp/init.lua
+++ nvim/nvim/lua/plugins/lsp/init.lua
@@ -198,5 +198,41 @@ return {
       --   end)
       -- end
     end,
-  }
+  },
+  {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        opts = {
+            pip = {
+                upgrade_pip = true,
+            },
+            ui = {
+                border = "rounded",
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("mason").setup(opts)
+            local utils = require("utils")
+            local mr = require("mason-registry")
+            local packages = utils.mason_packages
+            local function ensure_installed()
+                for _, package in ipairs(packages) do
+                    local p = mr.get_package(package)
+                    if not p:is_installed() then
+                        p:install()
+                    end
+                end
+            end
+            if mr.refresh then
+                mr.refresh(ensure_installed)
+            else
+                ensure_installed()
+            end
+        end,
+    },
 }
