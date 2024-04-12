89c8deb2784f1ae3d8b5220dd158c313243ef6f3
Ratheesh S <ratheeshreddy@gmail.com>
2024-04-12 10:23:17 +0530

    Add statuscol plugin

---
+++ nvim/nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,54 @@
+return {
+{
+		'luukvbaal/statuscol.nvim',
+		branch = '0.10',
+		enabled = true,
+    event = { "BufReadPre" },
+		config = function()
+			local builtin = require 'statuscol.builtin'
+			require('statuscol').setup {
+				-- setopt = true,
+				relculright = false,
+				clickhandlers = {
+					Lnum = builtin.gitsigns_click,
+				},
+				segments = {
+					{
+						sign = {
+							name = { '.*' },
+							namespace = { '.*' },
+							-- namespace = { ".*diagnostic.*" },
+							-- name = { "todo%-sign.*" }, -- WARN: escape the dash `-`
+							maxwidth = 1,
+							colwidth = 2,
+							auto = false,
+							wrap = true,
+						},
+					},
+					{
+						text = { ' ', builtin.lnumfunc, ' ' },
+						colwidth = 1,
+						click = 'v:lua.ScLa',
+					},
+					{
+						sign = {
+							name = { 'GitSigns*' },
+							namespace = { 'gitsigns' },
+							colwidth = 1,
+							fillchar = "│",
+							fillcharhl = 'StatusColLineChar',
+						},
+						click = 'v:lua.ScSa',
+					},
+					{
+						text = { builtin.foldfunc, ' ' },
+						hl = 'FoldColumn',
+						wrap = true,
+						colwidth = 1,
+						click = 'v:lua.ScFa',
+					},
+				},
+			}
+		end,
+	},
+}
