55d57f42fb31732c0b4c2bb50810c8e546e60b4a
Ratheesh S <ratheeshreddy@gmail.com>
2024-05-27 11:00:08 +0530

    lsp: remove extra documentation in the init module

--- nvim/nvim/lua/plugins/lsp/init.lua
+++ nvim/nvim/lua/plugins/lsp/init.lua
@@ -82,17 +82,12 @@ return {
         update_in_insert = false,
         severity_sort = false,
       },
-      -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-      -- Be aware that you also will need to properly configure your LSP server to
-      -- provide the inlay hints.
       inlay_hints = {
         enabled = true,
       },
-      -- add any global capabilities here
+
       capabilities = {},
-      -- options for vim.lsp.buf.format
-      -- `bufnr` and `filter` is handled by the LazyVim formatter,
-      -- but can be also overridden when specified
+
       format = {
         formatting_options = nil,
         timeout_ms = nil,
