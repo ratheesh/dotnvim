640f72d8b33c3c3270c052b90c05ae731cb74a21
Ratheesh S <ratheeshreddy@gmail.com>
2024-08-20 12:24:37 +0530

    Add nvim-osc52 plugin for handling osc52 calls

--- nvim/nvim/lua/plugins/editor.lua
+++ nvim/nvim/lua/plugins/editor.lua
@@ -102,4 +102,18 @@ return {
       },
     }
   },
+  {
+      'ojroques/nvim-osc52',
+      enabled = true,
+      event = "VeryLazy",
+      init = function()
+          local copy = function()
+              if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+                  require('osc52').copy_register('+')
+              end
+          end
+
+          vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
+      end
+  },
 }
