80be9e6adb43b0b6851eacb44029fc1afbdc3e37
Ratheesh S <ratheeshreddy@gmail.com>
2024-09-11 19:24:51 +0530

    lsp: Add shortcut to toggle inlay hints

--- nvim/nvim/lua/config/autocmds.lua
+++ nvim/nvim/lua/config/autocmds.lua
@@ -134,6 +134,23 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 }) ]]
 
+-- Toggle inline hints on a shortcut
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local bufnr = args.buf ---@type number
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.supports_method('textDocument/inlayHint') then
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+      vim.keymap.set('n', '<leader>i', function()
+        vim.lsp.inlay_hint.enable(
+          not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
+          { bufnr = bufnr }
+        )
+      end, { buffer = bufnr })
+    end
+  end,
+})
+
 -- Fix cursor jumps upon tab after non completion of previous snippet
 vim.api.nvim_create_autocmd('ModeChanged', {
   pattern = '*',
