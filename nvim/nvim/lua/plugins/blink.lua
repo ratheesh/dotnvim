10657883708e0fc6d1092324bbdd160ee54cca1d
Ratheesh S <ratheeshreddy@gmail.com>
2024-11-14 09:22:26 +0530

    Blink: Configure Tab/S-Tab keys to behave like nvim-cmp

--- nvim/nvim/lua/plugins/blink.lua
+++ nvim/nvim/lua/plugins/blink.lua
@@ -6,7 +6,39 @@ return {
 
     version = 'v0.*',
     opts = {
-      keymap = { preset = 'enter' },
+        keymap = {
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>'] = { 'hide', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' },
+
+            --[[ ['<Tab>'] = { 'snippet_forward', 'fallback' },
+            ['<S-Tab>'] = { 'snippet_backward', 'fallback' }, ]]
+
+            ['<Tab>'] = {
+                function(cmp)
+                    if cmp.is_in_snippet() then return cmp.snippet_forward()
+                    else return cmp.select_next() end
+                end,
+                'select_next',
+                'fallback'
+            },
+            ['<S-Tab>'] = {
+                function(cmp)
+                    if cmp.is_in_snippet() then return cmp.snippet_backward()
+                    else return cmp.select_prev() end
+                end,
+                'select_prev',
+                'fallback'
+            },
+
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-p>'] = { 'select_prev', 'fallback' },
+            ['<C-n>'] = { 'select_next', 'fallback' },
+
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        },
 
       highlight = {
         use_nvim_cmp_as_default = true,
