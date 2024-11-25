5eb60b192d810d9b9a111ddd4adcfff125436654
Ratheesh S <ratheeshreddy@gmail.com>
2024-11-25 21:27:49 +0530

    Add symbols.nvim

--- nvim/nvim/lua/plugins/lsp/init.lua
+++ nvim/nvim/lua/plugins/lsp/init.lua
@@ -38,6 +38,18 @@ return {
         end,
       },
       {
+        "oskarrrrrrr/symbols.nvim",
+        config = function()
+          local r = require("symbols.recipes")
+          require("symbols").setup(r.DefaultFilters, r.AsciiSymbols, {
+            -- custom settings here
+            -- e.g. hide_cursor = false
+          })
+          vim.keymap.set("n", "<leader>s", "<cmd>Symbols<CR>")
+          vim.keymap.set("n", "<leader>S", "<cmd>SymbolsClose<CR>")
+        end
+      },
+      {
         "luckasRanarison/clear-action.nvim",
         enabled = true,
         event = "LspAttach",
