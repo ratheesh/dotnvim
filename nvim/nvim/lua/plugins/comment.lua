98d1a8fc6b818292ef5c43cf3917689108bf672e
Ratheesh S <ratheeshreddy@gmail.com>
2024-12-06 10:40:44 +0530

    Adjust comment.nvim settings

--- nvim/nvim/lua/plugins/comment.lua
+++ nvim/nvim/lua/plugins/comment.lua
@@ -7,34 +7,31 @@ local M = {
 function M.config()
   require('Comment').setup({
     padding = true,
-    sticky  = true,
-    ignore  = nil,
+    sticky = true,
+    ignore = nil,
+
     toggler = {
       line = 'gcc',
-      block = 'gbb',
+      block = 'gbc',
     },
+
     opleader = {
-      line  = 'gc',
+      line = 'gc',
       block = 'gb',
     },
 
     extra = {
-      above = 'gcO',
-      below = 'gco',
-      eol   = '<A-;>',
+      above = 'gbO',
+      below = 'gbo',
+      eol = '<M-;>',
     },
 
     mappings = {
-      basic    = true,
-      extra    = true,
-      extended = false,
+      basic = true,
+      extra = true,
     },
-    pre_hook = function(ctx)
-      local line_start = (ctx.srow or ctx.range.srow) - 1
-      local line_end = ctx.erow or ctx.range.erow
-      -- require('lsp-inlayhints.core').clear(0, line_start, line_end)
-      vim.api.nvim_buf_clear_namespace(0, -1, line_start, line_end)
-    end,
+
+    pre_hook = nil,
     post_hook = nil,
   })
 end
