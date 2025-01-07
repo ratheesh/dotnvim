7e3b38c29463ef9dc9483e0b447fb01f0af5420d
Ratheesh S <ratheeshreddy@gmail.com>
2025-01-07 09:38:59 +0530

    Use homepage from snacks

--- nvim/nvim/lua/plugins/misc.lua
+++ nvim/nvim/lua/plugins/misc.lua
@@ -23,6 +23,36 @@ return {
       require("precognition").setup({})
     end
   },
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    enabled = true,
+    event   = "UIEnter",
+    opts    = {
+      bigfile = { enabled = true },
+      notifier = { enabled = true, timeout = 2000 },
+      dashboard = {
+        enabled = true,
+        formats = {
+          key = function(item)
+            return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+          end,
+        },
+        sections = {
+          { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
+          { title   = "MRU", padding = 1 },
+          { section = "recent_files", limit = 8, padding = 1 },
+          { title   = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+          { section = "recent_files", cwd = true, limit = 8, padding = 1 },
+          { title   = "Sessions", padding = 1 },
+          { section = "projects", padding = 1 },
+          { title   = "Bookmarks", padding = 1 },
+          { section = "keys" },
+        },
+
+      }
+    }
+  },
 }
 
 -- End of File
