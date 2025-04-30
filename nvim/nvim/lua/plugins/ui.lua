7fdb263d59d10d5644403e743223651b1040e293
Ratheesh S <ratheeshreddy@gmail.com>
2025-04-30 02:21:18 +0000

    nvim-highlight-colors: render as background color by default

--- nvim/nvim/lua/plugins/ui.lua
+++ nvim/nvim/lua/plugins/ui.lua
@@ -256,7 +256,7 @@ return {
       require("nvim-highlight-colors").setup {
         ---Render style
         ---@usage 'background'|'foreground'|'virtual'
-        render = 'virtual',
+        render = 'background',
 
         ---Set virtual symbol (requires render to be set to 'virtual')
         virtual_symbol = '■',
