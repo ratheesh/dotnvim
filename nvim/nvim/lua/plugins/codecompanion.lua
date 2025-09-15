f5862e1cb97689814bd8865fcdcc58eba1198b80
Ratheesh S <ratheeshreddy@gmail.com>
2025-09-15 20:05:51 +0530

    Add codecompanion ai plugin and configure for copilot

---
+++ nvim/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- NOTE: The log_level is in `opts.opts`
+      opts = {
+        log_level = "DEBUG", -- or "TRACE"
+        strategies = {
+          chat = {
+            adapter = "copilot",
+          },
+          inline = {
+            adapter = "copilot",
+          },
+          agent = {
+            adapter = "copilot",
+          },
+        },
+      },
+    },
+  },
+
+}
