608e63e806dc8382063e1e06a9348413b3b6f63a
Ratheesh <ratheeshreddy@gmail.com>
2026-01-19 10:58:30 +0530

    license: remove initials from my name

--- nvim/nvim/lua/plugins/init.lua
+++ nvim/nvim/lua/plugins/init.lua
@@ -145,7 +145,7 @@ return {
     cmd = { 'Gplv2', 'Apache', 'Mit' },
     config = function ()
       vim.g.licenses_copyright_holders_name = 'Ratheesh <ratheeshreddy@gmail.com>'
-      vim.g.licenses_authors_name           = 'Ratheesh S'
+      vim.g.licenses_authors_name           = 'Ratheesh'
       vim.g.licenses_default_commands       = { 'Gplv2', 'Apache', 'Mit' }
     end
   },
