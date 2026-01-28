923f6a23f844090ec2736ca7d8a01e2476bc0a69
Ratheesh <ratheeshreddy@gmail.com>
2026-01-28 10:13:10 +0530

    cmp: disable the functionality under certain conditions

--- nvim/nvim/lua/plugins/cmp.lua
+++ nvim/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,14 @@
 local M = {
   "hrsh7th/nvim-cmp",
-  enabled = true,
+  -- enabled = true,
+  enabled = function()
+    local disabled = false
+    disabled = disabled or (vim.api.nvim_get_option_value('buftype', { buf = 0 }) == 'prompt')
+    disabled = disabled or (vim.fn.reg_recording() ~= '')
+    disabled = disabled or (vim.fn.reg_executing() ~= '')
+    disabled = disabled or require('cmp.config.context').in_treesitter_capture('comment')
+    return not disabled
+  end,
   pin = false,
   lazy = true,
   event = { "InsertEnter", "CmdlineEnter" },
