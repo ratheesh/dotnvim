03a625a112b925a7de7f35b93ca1d6c5a2d403b3
Ratheesh <ratheeshreddy@gmail.com>
2026-04-17 10:46:42 +0530

    Add classlayout to visualize the classes and friends memory layout

---
+++ nvim/nvim/lua/plugins/langs.lua
@@ -0,0 +1,12 @@
+-- Copyright (c) 2026 Ratheesh <ratheeshreddy@gmail.com>. All Rights Reserved.
+-- License: MiT
+
+return {
+  {
+    "J-Cowsert/classlayout.nvim",
+    ft = { "c", "cpp" },
+    opts = {},
+  }
+}
+
+-- End of File
