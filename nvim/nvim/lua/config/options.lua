4ae26aad400aad7feb6e5733676dec5af738ee84
Ratheesh <ratheeshreddy@gmail.com>
2026-04-28 06:47:57 +0530

    refactor options to reduce startup time

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -2,80 +2,74 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- This file is automatically loaded by plugins.core
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Enable LazyVim auto format
-vim.g.autoformat = true
-
--- LazyVim root dir detection
--- Each entry can be:
--- * the name of a detector function like `lsp` or `cwd`
--- * a pattern or array of patterns like `.git` or `lua`.
--- * a function with signature `function(buf) -> string|string[]`
-vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 
 local opt = vim.opt
+local o = vim.o
 
-opt.autowrite = true -- Enable auto write
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 3 -- Hide * markup for bold and italic
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.cmdheight = 1
-opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
-opt.formatoptions = "jcroqlnt" -- tcqj
-opt.grepformat = "%f:%l:%c:%m"
-opt.grepprg = "rg --vimgrep"
-opt.ignorecase = true -- Ignore case
-opt.shada = "%,<50,'25,/25,:20,s20,h,f1"
-opt.inccommand = "nosplit" -- preview incremental substitute
-opt.laststatus = 3 -- global statusline
-opt.list = true -- Show some invisible characters (tabs...
-opt.mouse = "a" -- Enable mouse mode
-opt.number = true -- Print line number
-opt.pumblend = 0 -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.relativenumber = true -- Relative line numbers
-opt.scrolloff = 5 -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 4 -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8 -- Columns of context
-opt.colorcolumn = "81"
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = false -- Insert indents automatically
-opt.spell = true -- Enable spell
-opt.spelllang = { "en" }
-opt.splitbelow = true -- Put new windows below current
-opt.splitkeep = "screen"
-opt.splitright = true -- Put new windows right of current
-opt.tabstop = 4 -- Number of spaces tabs count for
-opt.termguicolors = true -- True color support
-opt.timeoutlen = 500
-opt.undofile = true
-opt.undoreload = 200
-opt.undolevels = 1000
-opt.updatetime = 250 -- Save swap file and trigger CursorHold
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5 -- Minimum window width
-opt.wrap = true -- Disable line wrap
-opt.showbreak='↪ '
-opt.listchars={
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.autoformat = true
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+
+o.autowrite = true
+o.clipboard = "unnamedplus"
+o.completeopt = "menu,menuone,noselect"
+o.conceallevel = 3
+o.confirm = true
+o.cmdheight = 1
+o.cursorline = true
+o.expandtab = true
+o.formatoptions = "jcroqlnt"
+o.grepformat = "%f:%l:%c:%m"
+o.grepprg = "rg --vimgrep"
+o.ignorecase = true
+o.shada = "%,<50,'25,/25,:20,s20,h,f1"
+o.inccommand = "nosplit"
+o.laststatus = 3
+o.list = true
+o.mouse = "a"
+o.number = true
+o.pumblend = 0
+o.pumheight = 10
+o.relativenumber = true
+o.scrolloff = 5
+o.shiftround = true
+o.shiftwidth = 4
+o.showmode = false
+o.sidescrolloff = 8
+o.colorcolumn = "81"
+o.signcolumn = "yes"
+o.smartcase = true
+o.smartindent = false
+o.spell = true
+o.spelllang = "en"
+o.splitbelow = true
+o.splitkeep = "screen"
+o.splitright = true
+o.tabstop = 4
+o.termguicolors = true
+o.timeoutlen = 500
+o.undofile = true
+o.undoreload = 200
+o.undolevels = 1000
+o.updatetime = 250
+o.virtualedit = "block"
+o.wildmode = "longest:full,full"
+o.winminwidth = 5
+o.wrap = true
+o.smoothscroll = true
+o.showbreak = '↪ '
+
+opt.listchars = {
   trail = "•",
   tab = "  ",
   extends = "",
   precedes = ""
 }
+
 opt.fillchars = {
   foldopen = "▼",
   foldclose = "▶",
-  -- fold = "⸱",
   fold = " ",
   foldsep = " ",
   diff = "╱",
@@ -85,11 +79,9 @@ opt.fillchars = {
 
 opt.guicursor = "n:block-nCursor-blinkon0,i-ci:ver30-iCursor-blinkon0,v-ve:block-vCursor-blinkon0,o:hor30-oCursor-blinkon0,r-cr:block-rCursor-blinkon0"
 
-if vim.fn.has("nvim-0.10") == 1 then
-  opt.smoothscroll = true
-end
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 
--- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
 -- End of File
