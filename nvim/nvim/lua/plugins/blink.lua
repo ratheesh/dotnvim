a5f0328745e057db721a1ee95ed7ca92747d5588
Ratheesh <ratheeshreddy@gmail.com>
2026-05-06 22:51:47 +0530

    blink: remove option to select item based on idx

--- nvim/nvim/lua/plugins/blink.lua
+++ nvim/nvim/lua/plugins/blink.lua
@@ -405,16 +405,6 @@ return {
         ["<Down>"] = { "select_next", "fallback" },
         ["<C-p>"]  = { "select_prev", "fallback" },
         ["<C-n>"]  = { "select_next", "fallback" },
-
-        ["<A-1>"] = { function(cmp) cmp.accept({ index = 1 }) end },
-        ["<A-2>"] = { function(cmp) cmp.accept({ index = 2 }) end },
-        ["<A-3>"] = { function(cmp) cmp.accept({ index = 3 }) end },
-        ["<A-4>"] = { function(cmp) cmp.accept({ index = 4 }) end },
-        ["<A-5>"] = { function(cmp) cmp.accept({ index = 5 }) end },
-        ["<A-6>"] = { function(cmp) cmp.accept({ index = 6 }) end },
-        ["<A-7>"] = { function(cmp) cmp.accept({ index = 7 }) end },
-        ["<A-8>"] = { function(cmp) cmp.accept({ index = 8 }) end },
-        ["<A-9>"] = { function(cmp) cmp.accept({ index = 9 }) end },
       }
 
       return opts
